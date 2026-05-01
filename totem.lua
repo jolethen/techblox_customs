682ce8af61fca9e32c680028af18c7688fd2748f
jolethen <155651557+jolethen@users.noreply.github.com>
2026-05-01 15:30:41 +0530

    Update totem.lua

--- totem.lua
+++ totem.lua
@@ -1,7 +1,8 @@
 -- Register the Totem Item
 minetest.register_craftitem("techblox:totem_of_undying", {
     description = "Techblox Totem of Undying\nPrevents death when held in inventory",
-    inventory_image = "techblox_totem.png",
+    -- Using the techblox green/purple design
+    inventory_image = "techblox_totem.png", 
     stack_max = 1,
 })
 
@@ -28,24 +29,43 @@ minetest.register_on_player_hpchange(function(player, hp_change, reason)
             local pos = player:get_pos()
             minetest.sound_play("default_recharge", {pos = pos, gain = 1.0, max_hear_distance = 16})
             
+            -- ORB 1 Spawner
             minetest.add_particlespawner({
-                amount = 60,
-                time = 0.1, -- Explode almost instantly
+                amount = 40,
+                time = 0.1,
                 minpos = {x = pos.x - 0.5, y = pos.y + 1, z = pos.z - 0.5},
                 maxpos = {x = pos.x + 0.5, y = pos.y + 1.5, z = pos.z + 0.5},
-                minvel = {x = -4, y = 2, z = -4},
-                maxvel = {x = 4, y = 6, z = 4},
-                minacc = {x = 0, y = -9.8, z = 0}, -- Gravity makes shards fall
-                maxacc = {x = 0, y = -9.8, z = 0},
+                minvel = {x = -3, y = 2, z = -3},
+                maxvel = {x = 3, y = 5, z = 3},
+                minacc = {x = 0, y = -5, z = 0},
+                maxacc = {x = 0, y = -5, z = 0},
                 minexptime = 1,
-                maxexptime = 2,
+                maxexptime = 1.5,
                 minsize = 1,
-                maxsize = 3,
-                texture = "techblox_totem_particle.png",
+                maxsize = 2,
+                texture = "orb1.png",
                 glow = 14,
             })
 
-            -- 4. Set HP to max (prevents the 'Death' state)
+            -- ORB 2 Spawner
+            minetest.add_particlespawner({
+                amount = 40,
+                time = 0.1,
+                minpos = {x = pos.x - 0.5, y = pos.y + 1, z = pos.z - 0.5},
+                maxpos = {x = pos.x + 0.5, y = pos.y + 1.5, z = pos.z + 0.5},
+                minvel = {x = -5, y = 3, z = -5},
+                maxvel = {x = 5, y = 6, z = 5},
+                minacc = {x = 0, y = -8, z = 0},
+                maxacc = {x = 0, y = -8, z = 0},
+                minexptime = 0.8,
+                maxexptime = 1.2,
+                minsize = 2,
+                maxsize = 4,
+                texture = "orb2.png",
+                glow = 10,
+            })
+
+            -- 4. Set HP to max
             local hp_max = player:get_properties().hp_max or 20
             player:set_hp(hp_max)
             
@@ -53,7 +73,7 @@ minetest.register_on_player_hpchange(function(player, hp_change, reason)
             minetest.chat_send_player(player:get_player_name(), 
                 " \n[Techblox] YOUR TOTEM HAS BEEN CONSUMED!\n ")
 
-            -- 6. CRITICAL: Return 0 to cancel the fatal damage
+            -- 6. Cancel the fatal damage
             return 0
         end
     end
